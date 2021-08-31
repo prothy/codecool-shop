9e9347e3521330c4066d7cb4a6fb44bf7fa93a3f
Balazs Toth <tothbalazs.contact@gmail.com>
2021-08-31 14:55:24 +0200

    feat: add Dropdown component

---
+++ src/main/frontend/src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dropdown() {
+	return (
+		<select name="sort" id="sort">
+			<option value="sort-by" disabled selected>Sort by</option>
+		</select>
+	)
+}
+
+export default Dropdown
