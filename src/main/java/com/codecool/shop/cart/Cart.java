224a937f195eeb0af0856c93a2daadafcf320e75
ellisman31 <akos99@protonmail.com>
2021-08-31 22:40:40 +0200

    feat (#21): checkProductPresent is checking the right object by product name and test with some temporary products in Initializer

--- src/main/java/com/codecool/shop/cart/Cart.java
+++ src/main/java/com/codecool/shop/cart/Cart.java
@@ -3,37 +3,58 @@ package com.codecool.shop.cart;
 import com.codecool.shop.model.products.Product;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Cart {
 
-    private HashMap<Product, Integer> content = new HashMap<>();
+    private Map<Product, Integer> content = new HashMap<>();
     private BigDecimal price;
 
-    Cart() {
+    public Cart() {
 
     }
 
     public void addProduct(Product product) {
 
-        if (content.containsKey(product)) {
-            int getQuantity = content.get(product);
+        AtomicBoolean isKeyPresent = checkProductPresent(product);
+
+        if (isKeyPresent.get()) {
+            int getQuantity = 1; //TODO: check the right amount of product
             content.put(product, getQuantity + 1);
-            calculatePrice(product, true, content.get(product));
+            calculatePrice(product, true, getQuantity);
         } else {
             content.put(product, 1);
-            calculatePrice(product, true, content.get(product));
+            calculatePrice(product, true, 1);
         }
 
         System.out.println("Item has been added to the card");
     }
 
+    private AtomicBoolean checkProductPresent(Product product) {
+
+        AtomicBoolean isKeyPresent= new AtomicBoolean(false);
+
+        Optional<Map.Entry<Product, Integer>> matchedEntry =
+                content.entrySet().stream().
+                                filter(element -> element.getKey().getName().equals(product.getName())).findAny();
+
+        matchedEntry.ifPresent(value -> {
+            isKeyPresent.set(true);
+        });
+
+        return isKeyPresent;
+
+    }
+
     public void removeProduct(Product product) {
 
-        if (content.containsKey(product) && content.get(product) > 1) {
-            int getQuantity = content.get(product);
+        AtomicBoolean isKeyPresent = checkProductPresent(product);
+
+        if (isKeyPresent.get()) {
+            int getQuantity = 1;
             content.remove(product, getQuantity - 1);
-            calculatePrice(product, false, content.get(product));
+            calculatePrice(product, false, getQuantity);
         } else {
             content.remove(product);
             calculatePrice(product, false, 0);
@@ -44,13 +65,15 @@ public class Cart {
 
     public void calculatePrice(Product product, boolean checkCart, int getQuantity) {
 
+        //TODO: save the previous item price
+
         String[] splitPrice = product.getPrice().split(" ");
-        BigDecimal getPrice = BigDecimal.valueOf(Long.parseLong(splitPrice[0]));
+        BigDecimal getPrice = new BigDecimal(splitPrice[0]);
 
         if (checkCart) {
             setCalculatedPrice(getPrice.multiply(BigDecimal.valueOf(getQuantity)));
         } else if (!checkCart && getQuantity > 1) {
-            setCalculatedPrice(getPrice.subtract(getPrice));
+            setCalculatedPrice(getPrice.multiply(BigDecimal.valueOf(getQuantity)).subtract(getPrice));
         } else if (!checkCart && getQuantity == 0){
             setCalculatedPrice(getPrice.subtract(getPrice));
         }
