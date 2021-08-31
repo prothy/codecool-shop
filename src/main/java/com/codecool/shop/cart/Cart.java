c93ce4488625c7e2357d64721c1c85ffe52f3f90
ellisman31 <akos99@protonmail.com>
2021-08-31 14:44:01 +0200

    feat (#21): calculatePrice by product and check the quantity for the right price

--- src/main/java/com/codecool/shop/cart/Cart.java
+++ src/main/java/com/codecool/shop/cart/Cart.java
@@ -17,10 +17,11 @@ public class Cart {
 
         if (content.containsKey(product)) {
             int getQuantity = content.get(product);
-            content.put(product, getQuantity+1);
-        }
-        else {
+            content.put(product, getQuantity + 1);
+            calculatePrice(product, true, content.get(product));
+        } else {
             content.put(product, 1);
+            calculatePrice(product, true, content.get(product));
         }
 
         System.out.println("Item has been added to the card");
@@ -30,16 +31,30 @@ public class Cart {
 
         if (content.containsKey(product) && content.get(product) > 1) {
             int getQuantity = content.get(product);
-            content.remove(product, getQuantity-1);
-        }
-        else {
+            content.remove(product, getQuantity - 1);
+            calculatePrice(product, false, content.get(product));
+        } else {
             content.remove(product);
+            calculatePrice(product, true, 0);
         }
 
         System.out.println("Item has been removed successfully!");
     }
 
-    public BigDecimal calculatePrice() {
-        return null;
+    public BigDecimal calculatePrice(Product product, boolean checkCart, int getQuantity) {
+
+        String[] splitPrice = product.getPrice().split(" ");
+        BigDecimal getPrice = BigDecimal.valueOf(Long.parseLong(splitPrice[0]));
+
+        if (checkCart) {
+            return getPrice.multiply(BigDecimal.valueOf(getQuantity));
+        } else if (getQuantity > 1) {
+            return getPrice.divide(BigDecimal.valueOf(getQuantity));
+        } else if (getQuantity == 0){
+            return getPrice.subtract(getPrice);
+        }
+        else {
+            return getPrice;
+        }
     }
 }
