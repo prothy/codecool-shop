3729483594eea04f1a7261d9f15ef5af2baaf6ff
ellisman31 <akos99@protonmail.com>
2021-08-31 15:00:03 +0200

    feat (#21): calculatePrice set the correct price and get by getCalculatedPrice method

--- src/main/java/com/codecool/shop/cart/Cart.java
+++ src/main/java/com/codecool/shop/cart/Cart.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 public class Cart {
 
     private HashMap<Product, Integer> content = new HashMap<>();
+    private BigDecimal price;
 
     Cart() {
 
@@ -41,20 +42,28 @@ public class Cart {
         System.out.println("Item has been removed successfully!");
     }
 
-    public BigDecimal calculatePrice(Product product, boolean checkCart, int getQuantity) {
+    public void calculatePrice(Product product, boolean checkCart, int getQuantity) {
 
         String[] splitPrice = product.getPrice().split(" ");
         BigDecimal getPrice = BigDecimal.valueOf(Long.parseLong(splitPrice[0]));
 
         if (checkCart) {
-            return getPrice.multiply(BigDecimal.valueOf(getQuantity));
-        } else if (getQuantity > 1) {
-            return getPrice.divide(BigDecimal.valueOf(getQuantity));
-        } else if (getQuantity == 0){
-            return getPrice.subtract(getPrice);
+            setCalculatedPrice(getPrice.multiply(BigDecimal.valueOf(getQuantity)));
+        } else if (!checkCart && getQuantity > 1) {
+            setCalculatedPrice(getPrice.subtract(getPrice));
+        } else if (!checkCart && getQuantity == 0){
+            setCalculatedPrice(getPrice.subtract(getPrice));
         }
         else {
-            return getPrice;
+            setCalculatedPrice(getPrice);
         }
+    }
+
+    public void setCalculatedPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getCalculatedPrice() {
+        return this.price;
     }
 }
