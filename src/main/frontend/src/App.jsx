49185096fe4e1ec5f47d8e4e5eabba29457604da
Balazs Toth <tothbalazs.contact@gmail.com>
2021-08-31 14:53:36 +0200

    fix: change code format

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import './App.css'
-import NavBar from './components/NavBar/NavBar'
+import Header from './components/Header/Header'
 import HomePage from './pages/HomePage'
 
 function App() {
-	const fallbackMessage = 'hello BELLO fallback'
 	return (
 		//Only one "parent" element can be rendered - you can use "fragments" like below
 		<>
-			<NavBar />
+			<Header/>
 			<Switch>
 				<Route exact path="/home">
-					<HomePage />
+					<HomePage/>
 				</Route>
 				<Route path="/">
-					<div>{fallbackMessage}</div>
+					<HomePage/>
 				</Route>
 			</Switch>
 		</>
