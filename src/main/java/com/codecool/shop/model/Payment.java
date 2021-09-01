0e934d95429e9069272c7cb33445c1488cf544af
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-01 13:18:41 +0200

    feat (#11): implement Payment abstract class

--- src/main/java/com/codecool/shop/model/Payment.java
+++ src/main/java/com/codecool/shop/model/Payment.java
@@ -1,10 +1,21 @@
 package com.codecool.shop.model;
 
+import java.util.HashMap;
+
 public class Payment {
-    private boolean success = true;
+    protected boolean success = false;
+    protected HashMap<String, String> details = new HashMap<>();
 
     public boolean isSuccess() {
         return success;
+    }
+
+    public HashMap<String, String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(HashMap<String, String> details) {
+        this.details = details;
     }
 
     public void setSuccess(boolean success) {
