58b1f22b1f80f01781dffe04c3133205528f0771
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-01 13:46:01 +0200

    feat (#37): implement UserDaoMem class

--- src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
@@ -3,19 +3,36 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.model.user.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDaoMem implements UserDao {
+    private List<User> data = new ArrayList<>();
+    private static UserDaoMem instance = null;
+
+    private UserDaoMem() {
+    }
+
+    public static UserDaoMem getInstance() {
+        if (instance == null) {
+            instance = new UserDaoMem();
+        }
+        return instance;
+    }
+
     @Override
     public void add(User user) {
-
+        user.setId(data.size() + 1);
+        data.add(user);
     }
 
     @Override
     public User find(int id) {
-        return null;
+        return data.stream().filter(data -> data.getId() == id).findFirst().orElse(null);
     }
 
     @Override
     public void remove(int id) {
-
+        data.remove(find(id));
     }
 }
