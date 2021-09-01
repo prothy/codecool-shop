3813d63556f75923c75f53e93d29fb8bb0889742
prothy <lukacsurgay@gmail.com>
2021-09-01 16:55:49 +0200

    feat: add quantity validation to ShoppingCartitem

--- src/main/frontend/src/components/ShoppingCartItem/ShoppingCartItem.jsx
+++ src/main/frontend/src/components/ShoppingCartItem/ShoppingCartItem.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ShoppingCartItem(props) {
-  console.log(props)
+  const [quantity, setQuantity] = useState(1)
+
+  function validateInput(ev) {
+    const value = parseInt(ev.target.value)
+    if (!Number.isInteger(value)) {
+      ev.target.value = ''
+    } else {
+      setQuantity(value)
+    }
+  }
+
   return (
     <div className="shopping-cart__product">
       <span className="shopping-cart__product-title">{props.data.name}</span>
       <span>
         <span className="shopping-cart__product-quantity">
-          <button>-</button>
-          <input type="number" min="1"></input>
-          <button>+</button>
+          <button onClick={() => setQuantity(quantity - 1)}>-</button>
+          <input onKeyPress={(ev) => validateInput(ev)} />
+          <button onClick={() => setQuantity(quantity + 1)}>+</button>
         </span>
-        <span className="shopping-cart__product-price"></span>
+        <span className="shopping-cart__product-price">${quantity * 500}</span>
       </span>
     </div>
   )
