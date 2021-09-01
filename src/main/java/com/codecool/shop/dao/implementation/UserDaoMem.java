acc7a4396fc888d26a1ca3bc2d721a1219cc420e
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-01 13:35:34 +0200

    feat (#37): create UserDaoMem class

---
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.user.User;
+
+public class UserDaoMem implements UserDao {
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public User find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+}
