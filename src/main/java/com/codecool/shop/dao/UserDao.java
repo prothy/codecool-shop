089c7e15090483f06e6b81807c568eceea39d253
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-01 16:12:58 +0200

    feat (#37): add createObjectFromJson() method in the UserDao interface

--- src/main/java/com/codecool/shop/dao/UserDao.java
+++ src/main/java/com/codecool/shop/dao/UserDao.java
@@ -3,10 +3,15 @@ package com.codecool.shop.dao;
 
 import com.codecool.shop.model.user.User;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface UserDao {
 
     void add(User user);
     User find(int id);
     void remove(int id);
+
+    List<List<User>> createObjectsFromJson() throws IOException;
 
 }
