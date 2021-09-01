20b5b269c15342fd3405729f7701a8a6b73b72e2
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-01 11:06:10 +0200

    feat (#37): add Order, Cart and Payment empty class for testing User class

---
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -0,0 +1,13 @@
+package com.codecool.shop.model;
+
+public class Cart {
+    String content = "content";
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/codecool/shop/model/Payment.java
@@ -0,0 +1,14 @@
+package com.codecool.shop.model;
+
+public class Payment {
+    private boolean success = true;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
+
