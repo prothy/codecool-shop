97cef005a1f1958a5c7ec9195e3ebe253f5d012f
prothy <lukacsurgay@gmail.com>
2021-09-01 14:59:23 +0200

    feat: add basic shopping cart layout, non-functional buttons

--- src/main/frontend/src/components/ShoppingCart/ShoppingCart.jsx
+++ src/main/frontend/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './ShoppingCart.css'
 
-function ShoppingCart() {
-  return <button className="shopping-cart">Shopping Cart</button>
+function ShoppingCart(jsonData) {
+  return (
+    <div className="shopping-cart">
+      <div>
+        {jsonData.data.map((el) => (
+          <div className="shopping-cart__product" key="{el.name}">
+            <span className="shopping-cart__product-title">{el.name}</span>
+            <span>
+              <span className="shopping-cart__product-quantity">
+                <button>+</button>
+                <input type="number" value="1"></input>
+                <button>-</button>
+              </span>
+              <span className="shopping-cart__product-price">{el.price}</span>
+            </span>
+          </div>
+        ))}
+      </div>
+      <button>Checkout</button>
+    </div>
+  )
 }
 
 export default ShoppingCart
--- src/main/frontend/src/pages/HomePage.jsx
+++ src/main/frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import Product from '../components/Product/Product'
 import SidePanel from '../components/SidePanel/SidePanel'
 import ShoppingCart from '../components/ShoppingCart/ShoppingCart'
@@ -20,19 +20,17 @@ function HomePage() {
   //     fetch
   // }, [])
 
-  const fetchApi = async () => {
-    return fetch('/api.noIDEaSHop', {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-  }
+  const [show, showCart] = useState(false)
 
-  useEffect(() => {
-    fetchApi()
-  }, [])
+  // const fetchApi = async () => {
+  //   return fetch('/api.noIDEaSHop', {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data))
+  // }
 
   return (
     <div className="product-page">
@@ -40,7 +38,15 @@ function HomePage() {
       <div>
         <div className="sort-cart-container">
           <Dropdown />
-          <ShoppingCart />
+          <div>
+            <button
+              className="shopping-cart__button"
+              onClick={() => showCart(show ? false : true)}
+            >
+              Shopping Cart
+            </button>
+            {show && <ShoppingCart data={fakesonData} />}
+          </div>
         </div>
         {/* You can write JS code in the return statement by wrapping it in {} */}
         <div className="product-card-container">
