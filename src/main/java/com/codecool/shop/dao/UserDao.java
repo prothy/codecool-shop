8214d99b0c135d137abbfd1210460bccb66d7f8d
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-01 13:35:03 +0200

    feat (#37): create UserDao interface

---
+++ src/main/java/com/codecool/shop/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.codecool.shop.dao;
+
+
+import com.codecool.shop.model.user.User;
+
+public interface UserDao {
+
+    void add(User user);
+    User find(int id);
+    void remove(int id);
+
+}
