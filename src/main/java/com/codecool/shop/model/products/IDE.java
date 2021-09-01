488fbc6e49e4c132a8536acd9d5601f2c2cb1f0c
martingrant01 <martin.nagy01@gmail.com>
2021-09-01 19:36:58 +0200

    feat (#22): modify classes in product package to have private fields, delete unused methods and delete constructors since they are not necessary

--- src/main/java/com/codecool/shop/model/products/IDE.java
+++ src/main/java/com/codecool/shop/model/products/IDE.java
@@ -8,26 +8,4 @@ import java.util.List;
 
 public class IDE extends SubscriptionProduct{
     private String languages;
-
-    public IDE(int id , String name, String description, ProductCategory productCategory, Supplier supplier, String image, BigDecimal price, BigDecimal yearlyPrice, BigDecimal monthlyPrice, String languages) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.productCategory = productCategory;
-        this.supplier = supplier;
-        this.image = image;
-        this.price = price;
-        this.yearlyPrice = yearlyPrice;
-        this.monthlyPrice = monthlyPrice;
-        this.setPrice(price, "USD");
-        this.languages = languages;
-    }
-
-    public String getLanguages() {
-        return languages;
-    }
-
-    public void setLanguages(String languages) {
-        this.languages = languages;
-    }
 }
--- src/main/java/com/codecool/shop/model/products/Product.java
+++ src/main/java/com/codecool/shop/model/products/Product.java
@@ -9,12 +9,12 @@ import java.util.Currency;
 
 public abstract class Product extends BaseModel {
 
-    protected Currency defaultCurrency;
-    protected String description;
-    protected ProductCategory productCategory;
-    protected Supplier supplier;
-    protected String image;
-    protected BigDecimal price;
+    private Currency defaultCurrency;
+    private String description;
+    private ProductCategory productCategory;
+    private Supplier supplier;
+    private String image;
+    private BigDecimal price;
 
     public ProductCategory getProductCategory() {
         return productCategory;
@@ -23,15 +23,6 @@ public abstract class Product extends BaseModel {
     public void setProductCategory(ProductCategory productCategory) {
         this.productCategory = productCategory;
         this.productCategory.addProduct(this);
-    }
-
-    public String getPrice() {
-        return price + " " + defaultCurrency.toString();
-    }
-
-    public void setPrice(BigDecimal price, String currency) {
-        this.price = price;
-        this.defaultCurrency = Currency.getInstance(currency);
     }
 
     public Supplier getSupplier() {
@@ -47,24 +38,4 @@ public abstract class Product extends BaseModel {
         return defaultCurrency;
     }
 
-//    public abstract BigDecimal getYearlyPrice();
-//
-//    public abstract void setYearlyPrice(BigDecimal yearlyPrice);
-//
-//    public abstract BigDecimal getMonthlyPrice();
-//
-//    public abstract void setMonthlyPrice(BigDecimal monthlyPrice);
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", defaultCurrency=" + defaultCurrency +
-                ", description='" + description + '\'' +
-                ", productCategory=" + productCategory +
-                ", supplier=" + supplier +
-                ", image='" + image + '\'' +
-                ", price=" + price +
-                '}';
-    }
 }
