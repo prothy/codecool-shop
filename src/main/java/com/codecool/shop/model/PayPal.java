9f97e2feb8e01fcfbeb886311a7b55819ed5bdd7
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-02 16:56:35 +0200

    chore (#11): move Payment.java, CreditCard.java and PayPal.java to the payment package

--- src/main/java/com/codecool/shop/model/PayPal.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.shop.model;
-
-import java.util.HashMap;
-
-public class PayPal extends Payment{
-    public PayPal(HashMap<String, String> details) {
-        super(details);
-    }
-}
