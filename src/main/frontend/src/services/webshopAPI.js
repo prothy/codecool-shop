37e5651bb4f01e59a14eeafed207c44691c65e3b
Balazs Toth <tothbalazs.contact@gmail.com>
2021-09-02 23:10:09 +0200

    feat: add getSuppliers api fetch

--- src/main/frontend/src/services/webshopAPI.js
+++ src/main/frontend/src/services/webshopAPI.js
@@ -8,7 +8,6 @@ export const getAllProducts = async () => {
 }
 
 export const getProductByCategory = async (productCategory) => {
-    console.log(productCategory)
     const response = await fetch(`/api.noIDEaSHop?category=${productCategory}`, {
         headers: {
             'Content-Type': 'application/json',
@@ -16,36 +15,45 @@ export const getProductByCategory = async (productCategory) => {
     })
     return await response.json()
 }
-// TODO: Implement fetch cart call:
+
+export const getSuppliers = async (supplier) => {
+    const response = await fetch(`/api.noIDEaSHop?supplier=${supplier}`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    return await response.json()
+}
+
 export const fetchCart = async () => {
-  const response = await fetch('/api.cart', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  return response.json()
+    const response = await fetch('/api.cart', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    return response.json()
 }
 
 export const addProductToCart = async (product) => {
-  await fetch('/api.cart?action=add', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: {
-      product: JSON.stringify(product),
-    },
-  })
+    await fetch('/api.cart?action=add', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: {
+            product: JSON.stringify(product),
+        },
+    })
 }
 
 export const removeProductToCart = async (product) => {
-  await fetch('/api.cart?action=remove', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: {
-      product: JSON.stringify(product),
-    },
-  })
+    await fetch('/api.cart?action=remove', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: {
+            product: JSON.stringify(product),
+        },
+    })
 }
