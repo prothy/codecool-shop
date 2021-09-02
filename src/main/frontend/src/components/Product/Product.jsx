76ec1f903ed011750131a8a955e05311cb04ba44
Balazs Toth <tothbalazs.contact@gmail.com>
2021-09-02 23:09:28 +0200

    feat: add Routes for Product

--- src/main/frontend/src/components/Product/Product.jsx
+++ src/main/frontend/src/components/Product/Product.jsx
@@ -1,58 +1,63 @@
 import React from 'react'
 import {
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  IconButton,
-  Typography,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    IconButton,
+    Typography,
 } from '@material-ui/core'
-import { AddShoppingCart, Favorite } from '@material-ui/icons'
+import {AddShoppingCart, Favorite} from '@material-ui/icons'
+import {Link} from 'react-router-dom'
 
 import useStyles from './styledProduct'
 
-const Product = ({ product, cart, setCart }) => {
-  const classes = useStyles()
+const Product = ({product, cart, setCart}) => {
+    const classes = useStyles()
 
-  const addToCart = () => {
-    setCart([...cart, product])
-    console.log(cart)
-  }
+    const addToCart = () => {
+        setCart([...cart, product])
+        console.log(cart)
+    }
 
-  return (
-    <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        image={product.image}
-        title={product.name}
-      />
-      <CardContent>
-        <div className={classes.cardContent}>
-          <Typography variant="body1" gutterBottom>
-            {product.name}
-          </Typography>
-          <Typography variant="body1">
-            {product.price} {product.defaultCurrency}
-          </Typography>
-        </div>
-        {/*TODO: Product description*/}
-        {/*<Typography variant='h2' color='textSecondary'>*/}
-        {/*    {product.description}*/}
-        {/*</Typography>*/}
-      </CardContent>
-      {/* Button functionality*/}
-      <CardActions disableSpacing className={classes.cardActions}>
-        {/*TODO: Add functionality to shopping cart button*/}
-        <IconButton aria-label="Add to Cart" onClick={() => addToCart()}>
-          <AddShoppingCart />
-        </IconButton>
-        <IconButton aria-label="Favorites">
-          {/*TODO: Add functionality to favorite button*/}
-          <Favorite />
-        </IconButton>
-      </CardActions>
-    </Card>
-  )
+    return (
+        <Card
+            className={classes.root}>
+            <Link to={`/products/${product.id}`}>
+                < CardMedia
+                    className={classes.media}
+                    image={product.image}
+                    title={product.name}
+                />
+            </Link>
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="body1" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="body1">
+                        {product.yearlyPrice} {product.price} {product.defaultCurrency}
+                    </Typography>
+                </div>
+                {/*TODO: Product description*/}
+                {/*<Typography variant='h2' color='textSecondary'>*/}
+                {/*    {product.description}*/}
+                {/*</Typography>*/}
+            </CardContent>
+            {/* Button functionality*/
+            }
+            <CardActions disableSpacing className={classes.cardActions}>
+                {/*TODO: Add functionality to shopping cart button*/}
+                <IconButton aria-label="Add to Cart" onClick={() => addToCart()}>
+                    <AddShoppingCart/>
+                </IconButton>
+                <IconButton aria-label="Favorites">
+                    {/*TODO: Add functionality to favorite button*/}
+                    <Favorite/>
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
 }
 
 export default Product
