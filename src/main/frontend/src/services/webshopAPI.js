ce01f358db604fa0268a4c1a1c45704a22d4708b
prothy <lukacsurgay@gmail.com>
2021-09-02 13:45:46 +0200

    feat (#7): add fetch cart api to frontend

--- src/main/frontend/src/services/webshopAPI.js
+++ src/main/frontend/src/services/webshopAPI.js
@@ -1,10 +1,41 @@
 export const getAllProducts = async () => {
-    const response = await fetch('/api.noIDEaSHop', {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    return await response.json()
+  const response = await fetch('/api.noIDEaSHop', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  return await response.json()
 }
 
 // TODO: Implement fetch cart call:
+export const fetchCart = async () => {
+  return await fetch('/api.cart', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+export const addProductToCart = async (product) => {
+  await fetch('/api.cart?action=add', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      product: product,
+    },
+  })
+}
+
+export const removeProductToCart = async (product) => {
+  await fetch('/api.cart?action=remove', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      product: product,
+    },
+  })
+}
