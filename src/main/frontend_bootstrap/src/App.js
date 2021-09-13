a862cf1f1d3f8c735a1cdab8103194b77cd49a13
toth-b <tothbalazs.contact@gmail.com>
2021-09-13 12:06:08 +0200

    Add necessary React files to the redesigned frontend

---
+++ src/main/frontend_bootstrap/src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+import {Container} from 'react-bootstrap'
+// Library Core:
+import {library} from '@fortawesome/fontawesome-svg-core'
+// Brand Icons:
+import {fab} from '@fortawesome/free-brands-svg-icons'
+// FontAwesome icons used in the project:
+import {faShoppingCart, faUser} from '@fortawesome/free-solid-svg-icons'
+// Collect the icons into the library:
+library.add(fab, faShoppingCart, faUser)
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header/>
+            <main className="py-3">
+                <Container>
+                    <Route path="/" component={HomePage} exact/>
+                    <Route path="/products/:id" component={ProductPage}/>
+                </Container>
+            </main>
+            <Footer/>
+        </BrowserRouter>
+    )
+}
+
+export default App
