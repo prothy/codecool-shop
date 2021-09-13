a4a778b1ffa345df103fef431bf7224d1498222c
toth-b <tothbalazs.contact@gmail.com>
2021-09-13 15:19:58 +0200

    feat: add redux state management to the homepage and loading effect

--- src/main/frontend_bootstrap/src/pages/HomePage.jsx
+++ src/main/frontend_bootstrap/src/pages/HomePage.jsx
@@ -1,29 +1,34 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import {useDispatch, useSelector} from 'react-redux'
+import {listProducts} from '../actions/productActions'
+
 
 function HomePage() {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList
 
     useEffect(() => {
-        async function fetchProducts() {
-            const {data} = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
 
     return (
         <div>
             <h1>Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product}/>
-                    </Col>
-                ))}
-            </Row>
+            {/* Handle loading or the error message upon loading problem */}
+            {loading ? <h2>Loading...</h2>
+                : error ? <h3>{error}</h3>
+                    :
+                    <Row>
+                        {products.map(product => (
+                            <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product}/>
+                            </Col>
+                        ))}
+                    </Row>
+            }
         </div>
     )
 }
