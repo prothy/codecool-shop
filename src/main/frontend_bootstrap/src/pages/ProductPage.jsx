6e50e433255080edfc4f900705318f45df406f4a
toth-b <tothbalazs.contact@gmail.com>
2021-09-13 16:48:42 +0200

    feat: rewrite ProductPage.jsx using Redux

--- src/main/frontend_bootstrap/src/pages/ProductPage.jsx
+++ src/main/frontend_bootstrap/src/pages/ProductPage.jsx
@@ -1,65 +1,72 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {listProductDetails} from '../actions/productActions'
 import {Link} from 'react-router-dom'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import {Row, Col, Image, ListGroup, Button, Card} from 'react-bootstrap'
-import axios from 'axios'
 
 function ProductPage({match}) {
-
-    const [product, setProduct] = useState([])
+    const dispatch = useDispatch()
+    const productDetails = useSelector(state => state.productDetails)
+    const {loading, error, product} = productDetails
 
     useEffect(() => {
-        async function fetchProduct() {
-            const {data} = await axios.get(`/api/products/${match.params.id}`)
-            setProduct(data)
-        }
-
-        fetchProduct()
-    }, [match.params.id])
+        dispatch(listProductDetails(match.params.id))
+    }, [dispatch, match])
 
 
     return (
         <div>
             <Link to="/" className="btn btn-dark my-3">Back</Link>
-            <Row>
-                {/* Image */}
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid/>
-                </Col>
+            {
+                loading ?
+                    <Loader/>
+                    : error
+                        ? <Message variant="danger">{error}</Message>
+                        : (
+                            <Row>
+                                {/* Image */}
+                                <Col md={6}>
+                                    <Image src={product.image} alt={product.name} fluid/>
+                                </Col>
 
-                <Col md={3}>
-                    <ListGroup variant="flush">
-                        {/* Name */}
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
+                                <Col md={3}>
+                                    <ListGroup variant="flush">
+                                        {/* Name */}
+                                        <ListGroup.Item>
+                                            <h3>{product.name}</h3>
+                                        </ListGroup.Item>
 
-                        {/* Description */}
-                        <ListGroup.Item>
-                            <span>Description: {product.description}</span>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
+                                        {/* Description */}
+                                        <ListGroup.Item>
+                                            <span>Description: {product.description}</span>
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                </Col>
 
-                {/* Price */}
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Price: </Col>
-                                    <Col>
-                                        <strong>{product.price} $</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
+                                {/* Price */}
+                                <Col md={3}>
+                                    <Card>
+                                        <ListGroup variant="flush">
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Price: </Col>
+                                                    <Col>
+                                                        <strong>{product.price} $</strong>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
 
-                            <ListGroup.Item className="d-grid">
-                                <Button className="btn-dark" type="button">Add to cart</Button>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+                                            <ListGroup.Item className="d-grid">
+                                                <Button className="btn-dark" type="button">Add to cart</Button>
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        )
+            }
         </div>
     )
 }
