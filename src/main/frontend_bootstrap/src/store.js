cb9e85ba65ab32ce6e26eea1fd5ea451ad359e81
toth-b <tothbalazs.contact@gmail.com>
2021-09-13 14:56:04 +0200

    feat: add Redux store to handle state

--- src/main/frontend_bootstrap/src/store.js
+++ src/main/frontend_bootstrap/src/store.js
@@ -1,7 +1,7 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import {productListReducer} from './reducers/productReducers'
+import {productListReducer} from './reducers/productReducer'
 
 const reducer = combineReducers({
     productList: productListReducer,
