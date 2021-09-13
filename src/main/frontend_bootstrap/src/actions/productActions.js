b725a297153a007b7f04163007db2343a00c4f8f
toth-b <tothbalazs.contact@gmail.com>
2021-09-13 16:47:34 +0200

    feat: add product detail function to productActions.js

--- src/main/frontend_bootstrap/src/actions/productActions.js
+++ src/main/frontend_bootstrap/src/actions/productActions.js
@@ -2,10 +2,14 @@ import axios from 'axios'
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 
-
+// Fetch the list of products:
 export const listProducts = () => async (dispatch) => {
     try {
         dispatch({type: PRODUCT_LIST_REQUEST})
@@ -19,6 +23,27 @@ export const listProducts = () => async (dispatch) => {
     } catch (e) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: e.response && e.response.data.message
+                ? e.response.data.message
+                : e.message
+        })
+    }
+}
+
+// Fetch the specific product information by id:
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({type: PRODUCT_DETAILS_REQUEST})
+
+        const {data} = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+    } catch (e) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: e.response && e.response.data.message
                 ? e.response.data.message
                 : e.message
