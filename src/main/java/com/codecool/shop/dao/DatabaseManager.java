aebab17a53ab73858035107e399a20b3f6dc2748
martingrant01 <martin.nagy01@gmail.com>
2021-09-13 16:31:10 +0200

    feat (#70): add Database handler to pom.xml, add DatabaseManager class with connection method

---
+++ src/main/java/com/codecool/shop/dao/DatabaseManager.java
@@ -0,0 +1,44 @@
+package com.codecool.shop.dao;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+public class DatabaseManager {
+
+    public void setup() throws SQLException {
+        Properties prop = new Properties();
+        String filename = "src/main/java/com/codecool/shop/resources/connection.properties";
+
+        try {
+            FileInputStream input = new FileInputStream(filename);
+
+            prop.load(input);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DataSource dataSource = connect(prop);
+
+    }
+
+    private DataSource connect(Properties prop) throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+        dataSource.setUser(prop.getProperty("user"));
+        dataSource.setPassword(prop.getProperty("password"));
+        dataSource.setDatabaseName(prop.getProperty("database"));
+
+        System.out.println("Trying to connect");
+        dataSource.getConnection().close();
+        System.out.println("Connection ok.");
+
+        return dataSource;
+    }
+}
--- src/main/java/com/codecool/shop/model/BaseModel.java
+++ src/main/java/com/codecool/shop/model/BaseModel.java
@@ -1,16 +1,29 @@
 package com.codecool.shop.model;
 
 
+import com.codecool.shop.dao.DatabaseManager;
+
 import java.lang.reflect.Field;
+import java.sql.SQLException;
 
 public class BaseModel {
 
     protected int id;
     protected String name;
+    protected DatabaseManager databaseManager;
 
     public BaseModel(int id, String name) {
         this.id = id;
         this.name = name;
+        this.databaseManager = new DatabaseManager();
+        setupDatabaseManager(databaseManager);
+    }
+
+    public BaseModel(int id, String name, DatabaseManager databaseManager) {
+        this.id = id;
+        this.name = name;
+        this.databaseManager = databaseManager;
+        setupDatabaseManager(databaseManager);
     }
 
     public int getId() {
@@ -45,6 +58,14 @@ public class BaseModel {
             }
         }
         return sb.toString();
+    }
+
+    private void setupDatabaseManager(DatabaseManager databaseManager) {
+        try {
+            databaseManager.setup();
+        } catch (SQLException ex) {
+            System.out.println("Cannot connect to database.");
+        }
     }
 
 }
