522adb99659819a22475db0387f297b5bfedc715
ellisman31 <akos99@protonmail.com>
2021-09-14 13:15:43 +0200

    feat(#59): create test folder with add product to cart and remove product from cart

---
+++ src/main/test/com/codecool/shop/model/cart/AddProductToCart.java
@@ -0,0 +1,13 @@
+package com.codecool.shop.model.cart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddProductToCart {
+
+    @Test
+    void addProduct() {
+    }
+
+}
