34da6d9bdf552407029b2d554c707725c7081784
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-14 14:42:27 +0200

    feat (#64): implement find() method in the UserDaoJdbc class

--- src/main/java/com/codecool/shop/dao/implementation/UserDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoJdbc.java
@@ -1,14 +1,14 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.cart.Cart;
+import com.codecool.shop.model.user.Admin;
 import com.codecool.shop.model.user.Customer;
 import com.codecool.shop.model.user.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.HashSet;
 
 public class UserDaoJdbc implements UserDao {
     private DataSource dataSource;
@@ -20,11 +20,12 @@ public class UserDaoJdbc implements UserDao {
     @Override
     public void add(User user) {
         try (Connection connection = dataSource.getConnection()){
-            String sql = "INSERT INTO users(email, password, isadmin, address, wallet, currency) VALUES (?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO users(user_name, email, password, isadmin, address, wallet, currency) VALUES (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, user.getEmail());
-            statement.setString(2, user.getPassword());
-            statement.setBoolean(3, user.isAdmin());
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.setBoolean(4, user.isAdmin());
             if (user instanceof Customer) {
                 statement.setString(4, ((Customer) user).getAddress());
                 statement.setBigDecimal(5, ((Customer) user).getWallet());
@@ -38,6 +39,38 @@ public class UserDaoJdbc implements UserDao {
 
     @Override
     public User find(int id) {
+        try (Connection connection = dataSource.getConnection()){
+            String sql = "SELECT * FROM users WHERE user_id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()){
+                return null;
+            }
+            if (resultSet.getBoolean(4)) {
+                User admin = new Admin(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4),
+                        resultSet.getBoolean(5));
+            } else {
+                User customer = new Customer(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4),
+                        resultSet.getBoolean(5),
+                        new Cart(), // [TODO]: CartJdbc needed
+                        new HashSet<>(), // [TODO]: OrderJdbc needed
+                        resultSet.getBigDecimal(7),
+                        resultSet.getString(8),
+                        resultSet.getString(9));
+            }
+
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
         return null;
     }
 
