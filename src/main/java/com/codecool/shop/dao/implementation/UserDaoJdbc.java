30fe4a2e9a91a3cdc6a8ede1193601647c121693
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-14 11:16:11 +0200

    feat (#64): implement add() method in the UserDaoJdbc class

--- src/main/java/com/codecool/shop/dao/implementation/UserDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoJdbc.java
@@ -1,11 +1,38 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.user.Customer;
 import com.codecool.shop.model.user.User;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class UserDaoJdbc implements UserDao {
+    private DataSource dataSource;
+
+    public UserDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void add(User user) {
+        try (Connection connection = dataSource.getConnection()){
+            String sql = "INSERT INTO users(email, password, isadmin, address, wallet, currency) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getPassword());
+            statement.setBoolean(3, user.isAdmin());
+            if (user instanceof Customer) {
+                statement.setString(4, ((Customer) user).getAddress());
+                statement.setBigDecimal(5, ((Customer) user).getWallet());
+                statement.setString(6, ((Customer) user).getDefaultCurrencyString());
+            }
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
 
     }
 
