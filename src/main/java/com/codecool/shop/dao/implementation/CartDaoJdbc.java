cf3f2bdae0d22e8e5d47f70c54c24b984c3bfeca
prothy <lukacsurgay@gmail.com>
2021-09-14 10:28:54 +0200

    feat (#10, #15): add empty dao interfaces + implementations for Cart and Order

---
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoJdbc.java
@@ -0,0 +1,6 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.CartDao;
+
+public class CartDaoJdbc implements CartDao {
+}
---
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoJdbc.java
@@ -0,0 +1,6 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.OrderDao;
+
+public class OrderDaoJdbc implements OrderDao {
+}
