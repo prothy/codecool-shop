7a072a5bba7a18f43f805566f350654d91acdbee
prothy <lukacsurgay@gmail.com>
2021-09-15 10:01:47 +0200

    feat (#91): add CartItem, begin implementing CartModel

--- src/main/java/com/codecool/shop/model/cart/CartModel.java
+++ src/main/java/com/codecool/shop/model/cart/CartModel.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class CartModel {
     private int userId;
     private int productId;
+    private List<CartItem> cart;
 
     public CartModel(int userId, int productId) {
         this.userId = userId;
@@ -31,6 +32,38 @@ public class CartModel {
 
     public void setProductId(int productId) {
         this.productId = productId;
+    }
+
+    public void addToCart(Product product) {
+        this.addToCart(product, 1);
+    }
+
+    public void addToCart(Product product, int quantity) {
+        boolean containsProduct = false;
+
+        for (CartItem cartItem : cart) {
+            if (cartItem.getProduct() == product) {
+                int currentQuantity = cartItem.getQuantity();
+                cartItem.setQuantity(currentQuantity + quantity);
+                containsProduct = true;
+            }
+        }
+
+        if (!containsProduct) {
+            cart.add(new CartItem(product, quantity));
+        }
+    }
+
+    public void removeAllFromCart(Product product) {
+
+    }
+
+    public void removeFromCart(Product product, int quantity) {
+
+    }
+
+    public List<CartItem> getCart() {
+        return cart;
     }
 }
 
