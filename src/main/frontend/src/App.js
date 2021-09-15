397be3dd1d650ea11e9d71146c6d0eb011bc7a72
toth-b <tothbalazs.contact@gmail.com>
2021-09-15 17:05:22 +0200

    feat: (#74) add RegisterPage to App.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
 import CartPage from './pages/CartPage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 import {Container} from 'react-bootstrap'
 // Library Core:
 import {library} from '@fortawesome/fontawesome-svg-core'
@@ -23,6 +24,7 @@ function App() {
                 <Container>
                     <Route path="/" component={HomePage} exact/>
                     <Route path="/login" component={LoginPage}/>
+                    <Route path="/register" component={RegisterPage}/>
                     <Route path="/products/:id" component={ProductPage}/>
                     <Route path="/cart/:id?" component={CartPage}/>
                 </Container>
