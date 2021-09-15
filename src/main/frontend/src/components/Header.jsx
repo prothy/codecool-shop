1d88341fb3ad3e88d775b199d11cab88733f8668
toth-b <tothbalazs.contact@gmail.com>
2021-09-15 14:09:41 +0200

    feat: (#74) add logout handler for Header.jsx

--- src/main/frontend/src/components/Header.jsx
+++ src/main/frontend/src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import {Container, Nav, Navbar} from 'react-bootstrap'
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {LinkContainer} from 'react-router-bootstrap'
+import {useDispatch, useSelector} from 'react-redux'
+import {logout} from '../actions/userActions'
 
 function Header() {
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -15,14 +27,24 @@ function Header() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            {/* Route for the shopping cart */}
                             <LinkContainer to="/cart">
                                 <Nav.Link><FontAwesomeIcon icon="shopping-cart"/> Cart</Nav.Link>
                             </LinkContainer>
-                            {/* Route for login */}
-                            <LinkContainer to="/login">
-                                <Nav.Link><FontAwesomeIcon icon="user"/> Login</Nav.Link>
-                            </LinkContainer>
+
+                            {userInfo ? (
+                                <NavDropdown id="username" title={userInfo.name}>
+                                    <LinkContainer to="/profile">
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link><FontAwesomeIcon icon="user"/> Login</Nav.Link>
+                                </LinkContainer>
+                            )}
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
