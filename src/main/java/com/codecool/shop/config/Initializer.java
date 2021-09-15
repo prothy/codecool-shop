98bc370780f291904d1227bdb6dda005134c2996
ellisman31 <akos99@protonmail.com>
2021-09-15 17:28:18 +0200

    feat: testing admin log feature with dummy data

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -6,12 +6,22 @@ import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.logger.AdminLog;
+import com.codecool.shop.logger.ProperLogMessages;
+import com.codecool.shop.model.OrderStatus;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
+import com.codecool.shop.model.cart.Cart;
 import com.codecool.shop.model.cart.CartModel;
+import com.codecool.shop.model.products.Cloud;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 @WebListener
 public class Initializer implements ServletContextListener {
@@ -26,6 +36,20 @@ public class Initializer implements ServletContextListener {
         Supplier amazon = new Supplier(1, "me");
         supplierDataStore.add(amazon);
         CartModel cart = new CartModel();
+
+        Cart testCart = new Cart();
+        Cloud cloud = new Cloud(0, "Google Drive", "", new ProductCategory(0, "test"),
+                new Supplier(0, "test"), "", new BigDecimal(0.1), new BigDecimal(0.1), new BigDecimal(0.1));
+        testCart.addProduct(cloud);
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        AdminLog logTest = new AdminLog(OrderStatus.NEW, 1, 0, timestamp, testCart);
+        try {
+            logTest.adminLogDetailsSaveIntoFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
