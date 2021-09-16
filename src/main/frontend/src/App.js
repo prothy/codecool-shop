78711712882d0c7348e27044f878d4ef74d8caf0
toth-b <tothbalazs.contact@gmail.com>
2021-09-16 09:45:18 +0200

    feat: add ShippingPage, PaymentPage, PlaceOrderPage and OrderPage to App.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -6,6 +6,10 @@ import ProductPage from './pages/ProductPage'
 import CartPage from './pages/CartPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import ShippingPage from './pages/ShippingPage'
+import PaymentPage from './pages/PaymentPage'
+import PlaceOrderPage from './pages/PlaceOrderPage'
+import OrderPage from './pages/OrderPage'
 import {Container} from 'react-bootstrap'
 // Library Core:
 import {library} from '@fortawesome/fontawesome-svg-core'
@@ -25,6 +29,10 @@ function App() {
                     <Route path="/" component={HomePage} exact/>
                     <Route path="/login" component={LoginPage}/>
                     <Route path="/register" component={RegisterPage}/>
+                    <Route path="/shipping" component={ShippingPage}/>
+                    <Route path="/placeorder" component={PlaceOrderPage}/>
+                    <Route path="/order/:id" component={OrderPage}/>
+                    <Route path="/payment" component={PaymentPage}/>
                     <Route path="/products/:id" component={ProductPage}/>
                     <Route path="/cart/:id?" component={CartPage}/>
                 </Container>
