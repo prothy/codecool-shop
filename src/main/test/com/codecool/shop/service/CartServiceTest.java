05a054641057b13ab3fa8a837682ce4a7ad694a2
prothy <lukacsurgay@gmail.com>
2021-09-16 13:10:01 +0200

    feat (#60): add removeFromCart test to reduce quantity to zero, which will remove the item from the cart

--- src/main/test/com/codecool/shop/service/CartServiceTest.java
+++ src/main/test/com/codecool/shop/service/CartServiceTest.java
@@ -16,7 +16,6 @@ import static org.mockito.Mockito.*;
 
 public class CartServiceTest {
     CartService cartService;
-    Product product;
     Product ide, os;
 
     @BeforeEach
@@ -51,6 +50,33 @@ public class CartServiceTest {
         assertEquals(cartService.getCart().size(), testCart.size());
         assertEquals(cartService.getCart().get(0).getProduct(), testCart.get(0).getProduct());
         assertEquals(cartService.getCart().get(1).getProduct(), testCart.get(1).getProduct());
+    }
+
+
+    @Test
+    void removeFromCartRemovesParticularQuantity() {
+        //Arrange
+        int quantity1 = (int) (Math.random() * 10);
+        int quantity2 = (int) (Math.random() * 10);
+
+        List<CartItem> testCart = new ArrayList<>();
+        CartItem testItem1 = new CartItem(ide, quantity1);
+        CartItem testItem2 = new CartItem(os, quantity2);
+
+        testCart.add(testItem1);
+
+        cartService.setCart(testCart);
+
+        testCart.add(testItem2);
+
+        //Act
+        cartService.addToCart(ide, quantity1);
+        cartService.addToCart(os, quantity2);
+        cartService.removeFromCart(os, quantity2);
+
+        //Assert
+        assertEquals(cartService.getCart().size(), testCart.size());
+        assertEquals(cartService.getCart().get(0).getProduct(), testCart.get(0).getProduct());
     }
 
 
